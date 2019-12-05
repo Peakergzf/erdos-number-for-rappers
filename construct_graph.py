a4130a5c4471b5243c7107f6751115d2fa1cdadd
peakergzf <peakergzf@outlook.com>
2019-12-06 00:02:05 +1100

    separate artists and genres files, modify the logic for adding edges and including artists

--- construct_graph.py
+++ construct_graph.py
@@ -1,10 +1,25 @@
 from spotify import sp
-import itertools
-from typing import Dict
 import networkx as nx
 import matplotlib.pyplot as plt
+import timeit
+import itertools
+from typing import Dict
 
 G = nx.Graph()
+GENRES = []
+ARTISTS = []
+
+
+def read_files():
+    global GENRES, ARTISTS
+
+    with open("genres.txt", "r") as f:
+        for line in f:
+            GENRES.append(line.rstrip())
+
+    with open("artists.txt", "r") as f:
+        for line in f:
+            ARTISTS.append(line.rstrip())
 
 
 def get_artist_id(name: str) -> str:
@@ -31,12 +46,22 @@ def get_artist_album_ids(artist_id: str) -> [str]:
 def get_album_featured_artists(album_id: str) -> [Dict[str, str]]:
     # the tracks in the album
     album_tracks: [Dict] = sp.album_tracks(album_id)["items"]
+
     # list of lists of featured artists
     artist_lst_lst: [[Dict]] = [track["artists"][1:] for track in album_tracks]
+
     # flatten the list (https://stackoverflow.com/a/716482)
     artist_lst: [Dict] = list(itertools.chain.from_iterable(artist_lst_lst))
+
     # filter the dictionary by key (https://stackoverflow.com/a/953097)
     return [{k: v for k, v in artist.items() if k in ["name", "id"]} for artist in artist_lst]
+
+
+def _include(artist_id):
+    # only include "mainstream" rappers in the graph
+    # (https://stackoverflow.com/a/17735466)
+    artist = sp.artist(artist_id)
+    return artist["popularity"] >= 85 and not set(artist["genres"]).isdisjoint(GENRES)
 
 
 def add_artist(artist: str) -> None:
@@ -57,21 +82,22 @@ def add_artist(artist: str) -> None:
         feat_id = featured_artist["id"]
         if feat_name.lower() == artist.lower():
             continue
-        if sp.artist(feat_id)["popularity"] < 85:
+        if not _include(feat_id):
             continue
 
         if G.has_node(feat_name) and G.has_edge(artist, feat_name):
             G[artist][feat_name]["weight"] += 1
-        elif not G.has_edge(artist, feat_name):
+        else:
             G.add_edge(artist, feat_name, weight=1)
             # add_artist(feat_name)
 
 
-def main():
-    artists = ["21 Savage", "Young Thug", "Travis Scott", "Migos", "Gucci Mane"]
-    for artist in artists:
+def construct_graph():
+    for artist in ARTISTS:
         add_artist(artist)
 
+
+def draw_graph():
     # (https://networkx.github.io/documentation/stable/reference/drawing.html#layout)
     # (https://networkx.github.io/documentation/stable/reference/generated/networkx.drawing.nx_pylab.draw_networkx.html)
     pos = nx.spring_layout(G)
@@ -83,6 +109,25 @@ def main():
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
     plt.axis('off')
     plt.show()
+
+
+def main():
+    start = timeit.default_timer()
+
+    read_files()
+    construct_graph()
+    print("{0:.2f}".format(timeit.default_timer() - start))
+    print("=" * 10)
+
+    # TODO the graph is "almost complete"
+    print(nx.center(G))
+    print(nx.eccentricity(G))
+
+    print("{0:.2f}".format(timeit.default_timer() - start))
+    print("=" * 10)
+
+    draw_graph()
+    print("{0:.2f}".format(timeit.default_timer() - start))
 
 
 if __name__ == '__main__':
