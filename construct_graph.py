d94d4e4b8d751dadb51aa67cf7e896038f82e233
peakergzf <peakergzf@outlook.com>
2020-04-13 23:31:14 +1000

    deal with connected component and add more comments

--- construct_graph.py
+++ construct_graph.py
@@ -8,18 +8,14 @@ import timeit
 import itertools
 from typing import Dict
 
-# TODO
-#  1. remove duplicate Jay-Z and Jay Z
-#  2. also search for singles
-#  3. adjust popularity threshold
-#  4. identify all connected components of the graph
-
-
 ccm = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=ccm)
 
+# collaboration graph of rappers
 G = nx.Graph()
+# list of hip-hop genres
 GENRES = []
+# list of rappers
 ARTISTS = []
 
 
@@ -58,6 +54,10 @@ def get_artist_album_ids(artist_id: str) -> [str]:
 
 
 def get_album_featured_artists(album_id: str) -> [Dict[str, str]]:
+    """
+    :param album_id
+    :return: a list of dictionary of artist name-id featured in the album
+    """
     # the tracks in the album
     album_tracks: [Dict] = sp.album_tracks(album_id)["items"]
 
@@ -73,19 +73,27 @@ def get_album_featured_artists(album_id: str) -> [Dict[str, str]]:
 
 def _include(artist_id):
     artist = sp.artist(artist_id)
-    # only include "mainstream" rappers in the graph
-    # check if two sets are disjoint https://stackoverflow.com/a/17735466
-    return artist["popularity"] >= 85 and not set(artist["genres"]).isdisjoint(GENRES)
+    # only include "mainstream" rappers in the graph (popularity >= 80)
+    # (check if two sets are disjoint https://stackoverflow.com/a/17735466)
+    return artist["popularity"] >= 80 and not set(artist["genres"]).isdisjoint(GENRES)
 
 
 def add_artist(artist_name: str, freq_cnt=False) -> None:
+    """
+    :param artist_name
+    :param freq_cnt:
+    True if the edge between two rappers represents the # of times they collaborated
+    False if the edge only indicates they collaborated or not
+    """
     if artist_name not in G:
         G.add_node(artist_name)
 
     artist_id = get_artist_id(artist_name)
     album_ids = get_artist_album_ids(artist_id)
 
+    # list of featured artists for each album
     featured_artist_lst_lst = [get_album_featured_artists(album_id) for album_id in album_ids]
+    # flatten the nested list
     featured_artist_lst = list(itertools.chain.from_iterable(featured_artist_lst_lst))
 
     for featured_artist in featured_artist_lst:
@@ -107,11 +115,18 @@ def add_artist(artist_name: str, freq_cnt=False) -> None:
 
 
 def construct_graph():
+    num_artists = len(ARTISTS)
+    cnt = 0
     for artist in ARTISTS:
         add_artist(artist)
+        cnt += 1
+        print(cnt, "/", num_artists)
 
 
 def draw_graph():
+    """
+    using networkx
+    """
     # https://networkx.github.io/documentation/stable/reference/drawing.html#layout
     # https://networkx.github.io/documentation/stable/reference/generated/networkx.drawing.nx_pylab.draw_networkx.html
     pos = nx.spring_layout(G)
@@ -126,6 +141,9 @@ def draw_graph():
 
 
 def vis_graph():
+    """
+    using pyvis
+    """
     # https://pyvis.readthedocs.io/en/latest/tutorial.html#example-visualizing-a-game-of-thrones-character-network
     nt = Network(height="750px", width="100%", bgcolor="#222222", font_color="white")
     nt.barnes_hut()
@@ -153,11 +171,37 @@ def main():
         artist = sp.artist(artist_id)
         print(artist["name"], artist["popularity"], artist["genres"])
 
+    dash_size = 50
+    print("=" * dash_size)
     time(construct_graph)
-    print(len(G))
-    if nx.is_connected(G):
-        print(nx.eccentricity(G))
-        print(nx.center(G))
+    print("=" * dash_size)
+
+    print(len(G), "rappers included.\n" + "=" * dash_size)
+
+    yt = "Young Thug"
+    for comp in nx.connected_components(G):
+        if len(comp) == 1:
+            continue
+
+        s = G.subgraph(comp)
+        print("eccentricity:\n", nx.eccentricity(s))
+
+        c = nx.center(s)
+        print("center:\n", c)
+
+        dists = nx.single_source_shortest_path_length(G, yt if yt in c else c[0])
+
+        print("distributions of distance:")
+        freq = {}
+        for _, d in dists.items():
+            if d not in freq:
+                freq[d] = 0
+            freq[d] += 1
+        print(freq)
+
+        print("distance from center:\n", dists)
+        print("-" * dash_size)
+
     vis_graph()
 
 
